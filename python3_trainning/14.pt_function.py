780e2d0320277745b8d5ac333ec031765247b44a
notready <loreley.lala@gmail.com>
2019-01-04 12:07:16 +0900

    関数の追加

---
+++ python3_trainning/14.pt_function.py
@@ -0,0 +1,20 @@
+#coding: UTF-8
+
+# ---------------------------------------
+# @note 関数
+# 定義　def functionName():
+# 呼び出し functionName()
+# ---------------------------------------
+
+# 定義
+def func1():
+    print("World")
+
+# func1()
+
+def func2():
+    print("Hello")
+    func1()
+
+# 呼び出し
+func2()
