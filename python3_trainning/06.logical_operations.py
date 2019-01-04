de97749c636780863fd7c8c560d088ff07640306
notready <notready@jp.notready>
2019-01-04 10:35:11 +0900

    論理演算子を追加

---
+++ python3_trainning/06.logical_operations.py
@@ -0,0 +1,26 @@
+#coding: UTF-8
+
+# ---------------------------------------
+# @note 論理演算子
+# - and / or / not
+# ---------------------------------------
+
+a = 5
+
+b = a > 4 and a < 6
+print(b)
+
+c = a < 4 and a > 5
+print(c)
+
+d = a < 3 or a > 4
+print(d)
+
+e = a < 3 or a > 6
+print(e)
+
+f = not a == 5
+print(f)
+
+g = not a == 4
+print(g)
